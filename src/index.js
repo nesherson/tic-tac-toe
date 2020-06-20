051a31ec66aeff657cb43ac73c5fd2ae751537f9
unknown <nesad.omeragich27@gmail.com>
2020-06-20 13:33:57 +0200

    added other player and reset functionality

--- src/index.js
+++ src/index.js
@@ -2,28 +2,62 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className='square' onClick={() => this.props.onClick()}>
-        {this.props.value}
-      </button>
-    );
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
   }
+  return null;
 }
+
+const Square = (props) => {
+  return (
+    <button className='square' onClick={() => props.onClick()}>
+      {props.value}
+    </button>
+  );
+};
 
 class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      activePlayer: 'X',
       squares: Array(9).fill(null),
     };
   }
 
   handleClick = (i) => {
-    const tempSquares = this.state.squares.slice();
-    tempSquares[i] = 'X';
-    this.setState({ squares: tempSquares });
+    const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) return;
+
+    if (this.state.squares[i] === null) {
+      const tempSquares = this.state.squares.slice();
+      tempSquares[i] = this.state.activePlayer;
+      this.setState({ squares: tempSquares });
+      this.setState({
+        activePlayer: this.state.activePlayer === 'X' ? 'O' : 'X',
+      });
+    }
+  };
+
+  handleReset = () => {
+    this.setState({
+      activePlayer: 'X',
+      squares: Array(9).fill(null),
+    });
   };
 
   renderSquare(i) {
@@ -36,7 +70,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner ' + winner;
+    } else {
+      status = `Next player: ${this.state.activePlayer}`;
+    }
 
     return (
       <div>
@@ -56,6 +96,7 @@ class Board extends React.Component {
           {this.renderSquare(7)}
           {this.renderSquare(8)}
         </div>
+        <button onClick={() => this.handleReset()}>Reset</button>
       </div>
     );
   }
